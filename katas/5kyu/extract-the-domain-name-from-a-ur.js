139303903352124b4165384d626889091bc9d71c
Alessia B <borys.alessia@gmail.com>
2022-10-26 15:16:48 +0100

    organising

---
+++ katas/5kyu/extract-the-domain-name-from-a-ur.js
@@ -0,0 +1,23 @@
+/**
+ * kata: Extract the domain name from a UR
+ *  kyu: 5
+ * date: 26/10/2022
+ * link: https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript 
+ * ==================================
+ * PROGRAM: 
+ * PARAMS: 
+ *      input: 
+ * returns: 
+ * ==================================
+ * 
+ *
+*/
+
+function extractDomainName(){
+
+}
+
+console.log("\n------name------")
+console.log(`expected to be: ${extractDomainName()} equal to: `)
+
+
